b4389d484c8a9426377856c3b75891f14d24632f
drachenss <drachens@duck.com>
2024-08-31 11:50:21 +0100

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("com.gradleup.shadow") version "8.3.0"
+    kotlin("jvm")
 }
 
 group = "org.drachens"
@@ -15,6 +16,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("org.spongepowered:configurate-yaml:4.1.2")
     implementation("com.google.code.gson:gson:2.11.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 java {
