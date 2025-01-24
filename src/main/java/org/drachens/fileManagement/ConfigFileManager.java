4e1ae40d6836b87e5c6293c72f3b2b55e1440c41
drachens1 <tpontont@gmail.com>
2025-01-24 18:08:45 +0000

    Permissions rework and optimisation

--- src/main/java/org/drachens/fileManagement/ConfigFileManager.java
+++ src/main/java/org/drachens/fileManagement/ConfigFileManager.java
@@ -2,9 +2,6 @@ package org.drachens.fileManagement;
 
 import org.drachens.fileManagement.customTypes.ServerPropertiesFile;
 import org.drachens.fileManagement.customTypes.WhitelistFile;
-import org.spongepowered.configurate.ConfigurateException;
-import org.spongepowered.configurate.ConfigurationNode;
-import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,17 +9,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ConfigFileManager {
-    private ConfigurationNode permissionsNode;
-    private YamlConfigurationLoader permissionsLoader;  // Has the permission groups that are created
     private String logMsg;
     private String logCmds;
-    private ServerPropertiesFile serverPropertiesFile;
-    private WhitelistFile whitelistFile;
+    private final ServerPropertiesFile serverPropertiesFile = new ServerPropertiesFile();
+    private final WhitelistFile whitelistFile = new WhitelistFile();
 
     public void startup() {
-        serverPropertiesFile = new ServerPropertiesFile();
-        whitelistFile = new WhitelistFile();
-
         //To setup all the log stuff
         System.out.println("Creating logs");
         new File("logs").mkdir();
@@ -39,17 +31,8 @@ public class ConfigFileManager {
         System.out.println("Logs finished");
 
         //Permissions
-        File permissions = new File("permissions.yml");
+        File permissions = new File("");
         fileExists(permissions);
-        permissionsLoader = YamlConfigurationLoader.builder()
-                .file(permissions)
-                .build();
-        try {
-            permissionsNode = permissionsLoader.load();
-        } catch (ConfigurateException e) {
-            System.err.println("Unable to load permissions " + e.getMessage());
-        }
-        createDefaultsPermissionLoader(permissionsNode);
     }
 
     private void fileExists(File f) {
@@ -75,30 +58,7 @@ public class ConfigFileManager {
         return whitelistFile;
     }
 
-    public void specificSave(String choice) {
-        System.out.println("Specifically saving: " + choice);
-        switch (choice) {
-            case "permissions":
-                try {
-                    permissionsLoader.save(permissionsNode);
-                } catch (ConfigurateException e) {
-                    System.err.println("Error saving permissions: " + e.getMessage());
-                }
-        }
-    }
-
     public ServerPropertiesFile getServerPropertiesFile() {
         return serverPropertiesFile;
-    }
-
-    private void createDefaultsPermissionLoader(ConfigurationNode permissionsNode) {
-        try {
-            if (permissionsNode.node("permissions").isNull()) {
-                permissionsNode.node("permissions").set(null);
-            }
-            permissionsLoader.save(permissionsNode);
-        } catch (ConfigurateException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
